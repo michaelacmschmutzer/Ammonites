b6f8c5d93c37f829529bf9ac561e33823ad5e081
michaelacmschmutzer <michael.acm.schmutzer@gmail.com>
2026-03-02 16:13:54 +0000

    Subsampling now works for species and genera

--- code/subsample_distributions.R
+++ code/subsample_distributions.R
@@ -10,12 +10,20 @@ source('geospatial-functions.R')
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0){
-  cephas <- 'nautilids'  # 'nautilids' 'ammonoids'
+  cephas <- 'ammonoids'  # 'nautilids' 'ammonoids'
+  taxon <- 'species' # 'species' 'genus'
 } else {
   cephas <- args[1]
+  taxon <- args[2]
 }
 
-cepha_rota <- read.csv('../data/cephalopods_palaeorotated.csv')
+# Import palaeorotated data
+if (taxon == 'genus') {
+  cepha_rota <- read.csv('../data/cephalopods_palaeorotated.csv')
+} 
+if (taxon == 'species') {
+  cepha_rota <- read.csv('../data/cephalopods_palaeorotated_species.csv')
+}
 
 # Get either nautilids or ammonoids
 if (cephas == 'nautilids') {
@@ -40,9 +48,9 @@ resolution <- c(25000)
 
 # Create data frames
 bootstrap.df <- data.frame(data.frame(matrix(ncol = 3, nrow = 0)))
-names(bootstrap.df) <- c('genus', 'area.km2', 'model')
+names(bootstrap.df) <- c(taxon, 'area.km2', 'model')
 jackknife.df <- data.frame(data.frame(matrix(ncol = 3, nrow = 0)))
-names(jackknife.df) <- c('genus', 'area.km2', 'model')
+names(jackknife.df) <- c(taxon, 'area.km2', 'model')
 
 for (m in rotamodels) {
   
@@ -54,25 +62,25 @@ for (m in rotamodels) {
   cornu_rota_geo <- cornu_rota %>% 
     filter(!is.na(!!sym(collng)) & !is.na(!!sym(collat)))
   
-  # Keep only those genera with more than three unique locations for each 
+  # Keep only those taxa with more than three unique locations for each 
   # reconstruction
-  tally_loc_genus <- cornu_rota_geo %>%
-    group_by(genus) %>%
+  tally_loc_taxon <- cornu_rota_geo %>%
+    group_by(.data[[taxon]]) %>%
     summarise(count = n_distinct(!!sym(collng), !!sym(collat)))
-  tally_loc_genus <- as.data.frame(tally_loc_genus)
-  genera <- tally_loc_genus %>% 
+  tally_loc_taxon <- as.data.frame(tally_loc_taxon)
+  taxa <- tally_loc_taxon %>% 
     filter(count > 3) %>%
-    pull(genus)
+    pull(.data[[taxon]])
   
-  for (g in genera) {
+  for (t in taxa) {
     
     # Get data for genus
-    genus <- cornu_rota_geo %>%
-      filter(genus == g)
+    taxon_data <- cornu_rota_geo %>%
+      filter(.data[[taxon]] == t)
     
     # Get unique occurrences within the cells of an equal-area grid
-    occ_uniq <- unique.within.grid(genus, collat, collng, 'EPSG:4326', eq_proj, 
-      resolution)
+    occ_uniq <- unique.within.grid(taxon_data, collat, collng, 'EPSG:4326',
+      eq_proj, resolution)
     
     # if the number of unique locations is more than three, do bootstrapping and
     # jackknifing on the unique occurrences. Perform only maxiter = 1000 
@@ -82,8 +90,10 @@ for (m in rotamodels) {
       jack.areas <- jackknife.area(occ_uniq, collat, collng, 'EPSG:4326')
       
       # append to data frames
-      bt.df <- data.frame(genus = g, area.km2 = boot.areas, model = m)
-      jk.df <- data.frame(genus = g, area.km2 = jack.areas, model = m)
+      bt.df <- data.frame(taxon = t, area.km2 = boot.areas, model = m)
+      jk.df <- data.frame(taxon = t, area.km2 = jack.areas, model = m)
+      names(bt.df)[names(bt.df) == 'taxon'] <- taxon
+      names(jk.df)[names(jk.df) == 'taxon'] <- taxon
       bootstrap.df <- rbind(bootstrap.df, bt.df)
       jackknife.df <- rbind(jackknife.df, jk.df)
     }
@@ -92,7 +102,8 @@ for (m in rotamodels) {
   
 }
 
-resdir <- paste('../results/subsampling_distributions/', cephas, '/', sep = '')
+resdir <- paste('../results/', taxon, '/subsampling_distributions/', cephas,
+  '/', sep = '')
 write.csv(bootstrap.df, paste(resdir, 'bootstrap.csv', sep = ''),
   row.names = FALSE)
 write.csv(jackknife.df,paste(resdir, 'jackknife.csv', sep = ''),
